d75126121bb478b0143e288e2d1647edb73558e5
saylorrain <43894548+saylorrain@users.noreply.github.com>
2019-03-21 08:38:18 -0500

    Add files via upload

---
+++ psuedo-code-jsfwpbc.js
@@ -0,0 +1,15 @@
+// Training Library
+
+
+// Display the available training replays
+// Show img, title, desc, register link
+// give each training a category, date
+// Set up search keywords for each training
+// include pagination of trainings 9 per view
+
+// Filter through the trainings based off date, alphabetical, category
+
+// Show quick view when clicked on a training
+// Allow modal to close when clicked anywhere off of 
+// Create overlay of bg when modal is open
+// Allow "page through" of trainings through the modal
